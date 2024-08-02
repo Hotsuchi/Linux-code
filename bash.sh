625e7089f93a54f96d2056b4225b9d252fa45576
Hotsuchi <suchitra38das@gmail.com>
2024-08-02 11:45:32 +0530

    add if condition

--- bash.sh
+++ bash.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-clear
-#bash file out put
-echo my first bash output command.
-#add variable
-name=tk
-age=34
-echo my name $name and my age $age.
-#add linux command in a variable
-pwd=$(pwd)
-echo $pwd
-#add input value in user
-echo Enter Your Age?
-read  userage
-echo user age $userage
+#clear
+##bash file out put
+#echo my first bash output command.
+##add variable
+#name=tk
+#age=34
+#echo my name $name and my age $age.
+##add linux command in a variable
+#pwd=$(pwd)
+#echo $pwd
+##add input value in user
+#echo Enter Your Age?
+#read  userage
+#echo user age $userage
+## add if conditino
+echo enter your age?
+read uage
+if [ $uage -ge 18 ]
+then
+    echo you are vote.
+else
+    echo you are not vote.
+fi
