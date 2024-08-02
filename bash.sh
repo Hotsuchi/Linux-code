5c23dc6cf495d8197201603bda712320f0dbb73b
Hotsuchi <suchitra38das@gmail.com>
2024-08-02 11:29:37 +0530

    add user input

--- bash.sh
+++ bash.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+clear
 #bash file out put
 echo my first bash output command.
 #add variable
 name=tk
 age=34
 echo my name $name and my age $age.
+#add linux command in a variable
+pwd=$(pwd)
+echo $pwd
+#add input value in user
+echo Enter Your Age?
+read  userage
+echo user age $userage
